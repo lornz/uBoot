c6a7139893c9906ba3d2262c38ddab7fbab00caa
Lorenz <lornz87@gmail.com>
2013-06-05 20:29:46 +0200

    "Create uBoot" added
    keine automatische Erstellung eines Spiels (war nur für debug)

--- scripts/debugLorenz.lua
+++ scripts/debugLorenz.lua
@@ -1,6 +1,6 @@
-gameChannel = "test"
-subscribe(gameChannel)
-timer.performWithDelay( 5000, chooseServer, 0)
+--gameChannel = "test"
+--subscribe(gameChannel)
+--timer.performWithDelay( 5000, chooseServer, 0)
 
 local testBoard = { elements = { 	sizeX = 20,	
 									position = 2,
@@ -9,29 +9,9 @@ local testBoard = { elements = { 	sizeX = 20,
 									skinID = 5,} 
 								}
 
-local function sendBoard()
-	print("connectionMode: "..connectionMode)
-	if not (connectionMode == 0) then
-		sendStuff(client1,"init",gameChannel,uuid)
-	end
-end
---timer.performWithDelay( 10000, sendBoard, 1)
 
+createUUID()
+lobbyChannel = "lobby_lorenz"
 
-local function sendReady()
-	--print("connectionMode: "..connectionMode)
-	--if not (connectionMode == 0) then
-		sendStuff(true,"ready",gameChannel)
-	--end
-end
-
---timer.performWithDelay( 10000, sendReady, 1)
-
---gameChannel = "test"
---subscribe(gameChannel)
---timer.performWithDelay( 5000, chooseServer, 0)
-
---timer.performWithDelay( 10000, sendReady, 1)
-
-lobbyChannel = "lobby"
-
+deviceID = (system.getInfo( "deviceID" ))
+print(deviceID)
