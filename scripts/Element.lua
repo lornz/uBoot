eea122da1eb5b9f54ea83d473e6c2751f6276dfa
Flo M <Temophor@gmail.com>
2013-06-05 19:29:53 +0200

    pumpe funktionalität implementiert
    beim pumpen wird pumpe.value verringert (und angezeigt)

--- scripts/Element.lua
+++ scripts/Element.lua
@@ -81,4 +81,58 @@ function displayImage(element)
 		end
 		button:addEventListener("tap", buttonTap)
 	end
+
+	if(s == 3) then
+		local sheetPumpe = sprite.newSpriteSheet( "media/gfx/roterKnopfSprite.png", 64, 64 )
+		local spriteSetPumpe = sprite.newSpriteSet(sheetPumpe, 1, 2)
+		sprite.add( spriteSetPumpe, "pumpe1", 1, 1, 1, 0 )
+		sprite.add( spriteSetPumpe, "pumpe2", 2, 1, 1, 0 )  
+		local pumpe = sprite.newSprite( spriteSetPumpe )
+		pumpe:prepare("pumpe1")
+		pumpe:play()       
+		pumpe.x = imageBackground.x + 64
+		pumpe.y = imageBackground.y + 64
+		pumpe.value = 0
+		pumpe.state = 0
+		pumpe.valueText = display.newText(pumpe.value, 0, 0, native.systemFont, 64)
+		pumpe.valueText:setReferencePoint(display.CenterReferencePoint)
+		pumpe.valueText.x = imageBackground.x + 192
+		pumpe.valueText.y = imageBackground.y + 64
+		pumpe.skinID = element.skinID
+
+		local function movePumpe(event) 
+			local t = event.target
+			local phase = event.phase
+			if(phase == "began") then
+				t.isFocus = true
+				t.x0 = event.x - t.x
+				t.y0 = event.y - t.y
+			elseif(t.isFocus) then
+				if "moved" == phase then
+					t.x = event.x - t.x0
+					--t.value = t.value - 1
+					if(t.x > imageBackground.x + 384 - 64) then
+						if(t.state == 0) then
+							t.value = t.value - 1
+						end
+						t.x = imageBackground.x + 384 - 64
+						t.state = 1
+						t.valueText.text = t.value
+					elseif(t.x < imageBackground.x + 64) then
+						if(t.state == 1) then
+							t.value = t.value - 1
+						end
+						t.x = imageBackground.x + 64
+						t.state = 0
+						t.valueText.text = t.value
+					end
+				end
+			elseif(phase == "ended" or phase == "cancelled") then
+				display.getCurrentStage():setFocus( nil )
+				t.isFocus      = false
+			end
+		end	
+
+		pumpe:addEventListener("touch", movePumpe)
+	end
 end
